6e907dccae04915b27c406f12c0f96ebfc0e4c04
Tamarack333 <pswilsonpro@gmail.com>
2025-06-22 11:36:10 -0400

    start downloading

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ const browserInstance = await puppeteer.launch({
 	headless: false
 })
 const page = await browserInstance.newPage()
+
+
+
 // loop through sites
 for(const ticker of tickers){
+	console.log(ticker)
 	// go to site
 	page.goto(`https://www.nasdaq.com/market-activity/stocks/${ticker}/historical?page=1&rows_per_page=10&timeline=y10`)
 	
@@ -17,13 +21,17 @@ for(const ticker of tickers){
  	await new Promise(resolve => setTimeout(resolve, 5000))
 	
 	if(`${page.url()}` === `https://www.nasdaq.com/market-activity/stocks/${ticker.toLowerCase()}/historical?page=1&rows_per_page=10&timeline=y10`){
-		console.log(`still on ${page.url()}`)
-		// download data with beneficial name and format	
-
-	}else{
-		console.log(`changed`)
+		// download data with beneficial name and format
+		// wrap in await promise
+		await page.waitForSelector('.historical-download')
+		console.log('Found Button')
+		// create promise for this where a failure appends the ticker to the failed file and a succsess logs confirmation
+		await page.click('.historical-download')
+		await new Promise(resolve => setTimeout(resolve, 5000))
 		// append ticker to file of all missed stocks. name file with date and time. 
-	}
-	}
+	}else{
+		// append ticker to failure file
+		console.log('Rediredted')}
+}
 browserInstance.close()
 
