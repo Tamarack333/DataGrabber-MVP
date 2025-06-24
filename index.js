292a576099221c7ce2e5ea2e39884d1fe734bc93
Tamarack333 <pswilsonpro@gmail.com>
2025-06-23 21:43:10 -0400

    create directories to organize and polish outline

--- index.js
+++ index.js
@@ -3,21 +3,32 @@ import puppeteer from "puppeteer"
 import fs from "fs"
 import { tickers } from './tickers.js'
 
-
 const browserInstance = await puppeteer.launch({
 	headless: false
 })
 const page = await browserInstance.newPage()
 
+function formattedDate(date) {
+	const day = String(date.getDate()).padStart(2, '0')
+	const month = String(date.getMonth() + 1).padStart(2, '0')
+	const year = date.getFullYear()
+	return `${day}/${month}/${year}`
+	// add time
+}
+// create parent directory
+let failureFile = `failures-${formattedDate(new Date())}`
+// create pass dir
+
 
 
 // loop through sites
 for(const ticker of tickers){
+	// let timestamp = new Date()
 	console.log(ticker)
 	// go to site
 	page.goto(`https://www.nasdaq.com/market-activity/stocks/${ticker}/historical?page=1&rows_per_page=10&timeline=y10`)
 	
-	// wait for .5 second
+
  	await new Promise(resolve => setTimeout(resolve, 5000))
 	
 	if(`${page.url()}` === `https://www.nasdaq.com/market-activity/stocks/${ticker.toLowerCase()}/historical?page=1&rows_per_page=10&timeline=y10`){
